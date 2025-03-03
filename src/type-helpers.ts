e276643228bb40e69d81f9c07281d1b4c845f9db
Luca Schnellmann <oss@lusc.ch>
2025-03-03 14:48:50 +0100

    fix: `PartialDeep` now supports types with `?:` values

--- src/type-helpers.ts
+++ src/type-helpers.ts
@@ -15,15 +15,26 @@ type IsPrimitive<T> = T extends number | boolean | string | null | undefined
 	? true
 	: false;
 
+// Separate or readability
+// Cannot handle unions, must receive a single type
+type PartialDeepUnionValues<T> =
+	IsPrimitive<T> extends true
+		? T
+		: T extends Array<infer V>
+			? Array<PartialDeep<V>>
+			: T extends ReadonlyArray<infer V>
+				? ReadonlyArray<PartialDeep<V>>
+				: T extends Record<string, unknown>
+					? PartialDeep<T>
+					: never;
+
 // No support for tuples
 export type PartialDeep<T> = {
-	[Key in keyof T]?: IsPrimitive<T[Key]> extends true
-		? T[Key]
-		: T[Key] extends Array<infer V>
-			? Array<PartialDeep<V>>
-			: T[Key] extends ReadonlyArray<infer V>
-				? ReadonlyArray<PartialDeep<V>>
-				: T[Key] extends Record<string, unknown>
-					? PartialDeep<T[Key]>
-					: never;
+	// If T[Key] is a union or optional with `?:`
+	// It is necessary to handle each value individually
+	// This is one way of doing that
+	// With `extends` TS runs it seperately for each union item
+	[Key in keyof T]?: T[Key] extends infer V1
+		? PartialDeepUnionValues<V1>
+		: never;
 };
